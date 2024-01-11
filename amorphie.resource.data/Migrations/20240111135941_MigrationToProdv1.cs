dc8dfff7a37d5b1d9c9bf70be775d81b085ad00b
tsimsekburgan <TSimsek@burgan.com.tr>
2024-01-11 17:00:18 +0300

    One Migration

--- amorphie.resource.data/Migrations/20240111135941_MigrationToProdv1.cs
+++ amorphie.resource.data/Migrations/20240111135941_MigrationToProdv1.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace amorphie.resource.data.Migrations
 {
     /// <inheritdoc />
-    public partial class initializeDB : Migration
+    public partial class MigrationToProdv1 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -17,6 +17,7 @@ namespace amorphie.resource.data.Migrations
                 {
                     Id = table.Column<Guid>(type: "uuid", nullable: false),
                     Url = table.Column<string>(type: "text", nullable: true),
+                    Type = table.Column<byte>(type: "smallint", nullable: false),
                     CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
                     CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
                     CreatedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true),
@@ -30,13 +31,12 @@ namespace amorphie.resource.data.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "ResourcePrivileges",
+                name: "ResourceClients",
                 columns: table => new
                 {
                     Id = table.Column<Guid>(type: "uuid", nullable: false),
                     ResourceId = table.Column<Guid>(type: "uuid", nullable: false),
-                    PrivilegeId = table.Column<Guid>(type: "uuid", nullable: false),
-                    Ttl = table.Column<int>(type: "integer", nullable: true),
+                    ClientId = table.Column<Guid>(type: "uuid", nullable: false),
                     Status = table.Column<string>(type: "text", nullable: true),
                     CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
                     CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
@@ -47,7 +47,66 @@ namespace amorphie.resource.data.Migrations
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_ResourcePrivileges", x => x.Id);
+                    table.PrimaryKey("PK_ResourceClients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResourceGroupResources",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResourceGroupId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResourceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceGroupResources", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResourceGroupRoles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResourceGroupId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RoleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceGroupRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResourceGroups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Tags = table.Column<string[]>(type: "text[]", nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceGroups", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
@@ -74,26 +133,6 @@ namespace amorphie.resource.data.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "ResourceRoles",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    ResourceId = table.Column<Guid>(type: "uuid", nullable: false),
-                    RoleId = table.Column<Guid>(type: "uuid", nullable: false),
-                    Status = table.Column<string>(type: "text", nullable: true),
-                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
-                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
-                    CreatedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true),
-                    ModifiedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
-                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
-                    ModifiedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ResourceRoles", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
                 name: "Resources",
                 columns: table => new
                 {
@@ -112,6 +151,27 @@ namespace amorphie.resource.data.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Resources", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResponseTransformations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResponseCode = table.Column<string>(type: "text", nullable: true),
+                    Filter = table.Column<string>(type: "text", nullable: true),
+                    Audience = table.Column<string[]>(type: "text[]", nullable: true),
+                    DisplayMode = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResponseTransformations", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
@@ -194,12 +254,68 @@ namespace amorphie.resource.data.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "ResourcePrivileges",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResourceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PrivilegeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: true),
+                    Priority = table.Column<int>(type: "integer", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourcePrivileges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResourcePrivileges_Privileges_PrivilegeId",
+                        column: x => x.PrivilegeId,
+                        principalTable: "Privileges",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResponseTransformationMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ResponseTransformationId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Language = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Subtitle = table.Column<string>(type: "text", nullable: true),
+                    Icon = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedByBehalfOf = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResponseTransformationMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResponseTransformationMessages_ResponseTransformations_Resp~",
+                        column: x => x.ResponseTransformationId,
+                        principalTable: "ResponseTransformations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "Translations",
                 columns: table => new
                 {
                     Id = table.Column<Guid>(type: "uuid", nullable: false),
                     Language = table.Column<string>(type: "text", nullable: false),
                     Label = table.Column<string>(type: "text", nullable: false),
+                    ResourceGroupId_Title = table.Column<Guid>(type: "uuid", nullable: true),
                     ResourceId_Description = table.Column<Guid>(type: "uuid", nullable: true),
                     ResourceId_DisplayName = table.Column<Guid>(type: "uuid", nullable: true),
                     RoleGroupId_Title = table.Column<Guid>(type: "uuid", nullable: true),
@@ -215,6 +331,11 @@ namespace amorphie.resource.data.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Translations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Translations_ResourceGroups_ResourceGroupId_Title",
+                        column: x => x.ResourceGroupId_Title,
+                        principalTable: "ResourceGroups",
+                        principalColumn: "Id");
                     table.ForeignKey(
                         name: "FK_Translations_Resources_ResourceId_Description",
                         column: x => x.ResourceId_Description,
@@ -241,6 +362,21 @@ namespace amorphie.resource.data.Migrations
                         principalTable: "Scopes",
                         principalColumn: "Id");
                 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourcePrivileges_PrivilegeId",
+                table: "ResourcePrivileges",
+                column: "PrivilegeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResponseTransformationMessages_ResponseTransformationId",
+                table: "ResponseTransformationMessages",
+                column: "ResponseTransformationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translations_ResourceGroupId_Title",
+                table: "Translations",
+                column: "ResourceGroupId_Title");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Translations_ResourceId_Description",
@@ -272,7 +408,13 @@ namespace amorphie.resource.data.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Privileges");
+                name: "ResourceClients");
+
+            migrationBuilder.DropTable(
+                name: "ResourceGroupResources");
+
+            migrationBuilder.DropTable(
+                name: "ResourceGroupRoles");
 
             migrationBuilder.DropTable(
                 name: "ResourcePrivileges");
@@ -281,13 +423,22 @@ namespace amorphie.resource.data.Migrations
                 name: "ResourceRateLimits");
 
             migrationBuilder.DropTable(
-                name: "ResourceRoles");
+                name: "ResponseTransformationMessages");
 
             migrationBuilder.DropTable(
                 name: "RoleGroupRoles");
 
             migrationBuilder.DropTable(
                 name: "Translations");
+
+            migrationBuilder.DropTable(
+                name: "Privileges");
+
+            migrationBuilder.DropTable(
+                name: "ResponseTransformations");
+
+            migrationBuilder.DropTable(
+                name: "ResourceGroups");
 
             migrationBuilder.DropTable(
                 name: "Resources");
