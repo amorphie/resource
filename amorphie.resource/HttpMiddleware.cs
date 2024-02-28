bd92ed1dc5b6974e32fc537ab2d85067a185565b
Sercan Yüksel <srcanyuksel@gmail.com>
2024-02-28 14:09:56 +0300

    fix

--- amorphie.resource/HttpMiddleware.cs
+++ amorphie.resource/HttpMiddleware.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IO;
 
 namespace amorphie.resource
 {
@@ -19,9 +20,14 @@ namespace amorphie.resource
             try
             {
                 context.Request.EnableBuffering();
-                string requestBody = await new StreamReader(context.Request.Body, Encoding.UTF8).ReadToEndAsync();
+                RecyclableMemoryStreamManager _recyclableMemoryStreamManager = new();
+                await using var requestStream = _recyclableMemoryStreamManager.GetStream();
+
+                await context.Request.Body.CopyToAsync(requestStream);
+                var body = ReadStreamInChunks(requestStream);
                 context.Request.Body.Position = 0;
-                Console.WriteLine("Http Request Body : "+requestBody);
+
+                Console.WriteLine("Http Request Body : "+body);
                 Console.WriteLine("----Headers----");
                 foreach(var h in context.Request.Headers)
                 {
@@ -35,6 +41,24 @@ namespace amorphie.resource
             }
             
 
+        }
+
+        private string ReadStreamInChunks(Stream stream)
+        {
+            const int readChunkBufferLength = 4096;
+            stream.Seek(0, SeekOrigin.Begin);
+            using var textWriter = new StringWriter();
+            using var reader = new StreamReader(stream);
+            var readChunk = new char[readChunkBufferLength];
+            int readChunkLength;
+            do
+            {
+                readChunkLength = reader.ReadBlock(readChunk,
+                                                   0,
+                                                   readChunkBufferLength);
+                textWriter.Write(readChunk, 0, readChunkLength);
+            } while (readChunkLength > 0);
+            return textWriter.ToString();
         }
            
     }
