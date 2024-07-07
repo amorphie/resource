201e116e3d703f49de7dba6e63fad2b140d3e7bb
Sercan Yüksel <srcanyuksel@gmail.com>
2024-07-07 16:24:26 +0300

    fix

---
+++ amorphie.resource.data/Migrations/20240707132416_RoleDefinitionKeyType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace amorphie.resource.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleDefinitionKeyType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Key",
+                table: "RoleDefinitions",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "RoleDefinitions",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
