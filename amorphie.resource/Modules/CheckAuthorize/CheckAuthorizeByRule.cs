7ab1b2527214b58bad377357c10d596f5c8cfd76
Tayfun Yılmaz <tayfunyilmas@gmail.com>
2024-07-16 17:29:42 +0300

    not using method removed

--- amorphie.resource/Modules/CheckAuthorize/CheckAuthorizeByRule.cs
+++ amorphie.resource/Modules/CheckAuthorize/CheckAuthorizeByRule.cs
@@ -107,151 +107,15 @@ public class CheckAuthorizeByRule : CheckAuthorizeBase, ICheckAuthorize
         {
             var jsonObject = JsonConvert.DeserializeObject<JObject>(request.Data);
             bodyParamList = MapHelper.ToDictionary(jsonObject);
-            // RecursiveJsonLoop(jsonObject, bodyParamList, "");
         }
 
         dynamic bodyObject = MapHelper.ToExpandoObject(bodyParamList);
-        // dynamic bodyObject = CreateExpandoObject(bodyParamList);
 
         var ruleParamBody = new RuleParameter("body", bodyObject);
 
         ruleParams.Add(ruleParamBody);
     }
-
-    dynamic CreateExpandoObject(Dictionary<string, object> properties)
-    {
-        ExpandoObject expando = new ExpandoObject();
-
-        try
-        {
-            IDictionary<string, object> dictionary = expando;
-
-            foreach (var property in properties)
-            {
-                string propertyName = property.Key;
-                object value = property.Value;
-
-                string[] parts = propertyName.Split('.');
-                CreateExpandoNestedObjects(dictionary, parts, value);
-            }
-        }
-        catch (Exception ex)
-        {
-        }
-
-        return expando;
-    }
-
-    void CreateExpandoNestedObjects(IDictionary<string, object> dictionary, string[] parts, object value)
-    {
-        IDictionary<string, object> currentDict = dictionary;
-
-        for (int i = 0; i < parts.Length; i++)
-        {
-            string part = parts[i];
-            bool isArray = part.Contains("[") && part.Contains("]");
-            int index = -1;
-
-            if (isArray)
-            {
-                int startIndex = part.IndexOf('[');
-                int endIndex = part.IndexOf(']');
-                index = int.Parse(part.Substring(startIndex + 1, endIndex - startIndex - 1));
-                part = part.Substring(0, startIndex);
-            }
-
-            if (!currentDict.ContainsKey(part))
-            {
-                if (i == parts.Length - 1) // Last part, assign value
-                {
-                    if (isArray)
-                    {
-                        List<object> list = new List<object>();
-                        currentDict.Add(part, list);
-                        for (int j = 0; j <= index; j++)
-                        {
-                            if (j == index)
-                            {
-                                list.Add(value);
-                            }
-                            else
-                            {
-                                list.Add(null);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        currentDict.Add(part, value);
-                    }
-                }
-                else
-                {
-                    if (isArray)
-                    {
-                        List<object> list = new List<object>();
-                        currentDict.Add(part, list);
-                        IDictionary<string, object> nestedDict = new ExpandoObject();
-                        list.Add(nestedDict);
-                        currentDict = nestedDict;
-                    }
-                    else
-                    {
-                        IDictionary<string, object> nestedDict = new ExpandoObject();
-                        currentDict.Add(part, nestedDict);
-                        currentDict = nestedDict;
-                    }
-                }
-            }
-            else
-            {
-                if (isArray && i == parts.Length - 1) // Existing list, assign value
-                {
-                    var list = (List<object>)currentDict[part];
-                    while (list.Count <= index)
-                    {
-                        list.Add(null);
-                    }
-
-                    list[index] = value;
-                }
-
-                if (currentDict[part] is IDictionary<string, object> dict)
-                {
-                    currentDict = dict;
-                }
-                else if (currentDict[part] is List<object> list)
-                {
-                    IDictionary<string, object> newDict = new Dictionary<string, object>();
-
-                    foreach (var item in list)
-                    {
-                        if (item is IDictionary<string, object> itemDict)
-                        {
-                            foreach (var kvp in itemDict)
-                            {
-                                if (!newDict.ContainsKey(kvp.Key))
-                                {
-                                    newDict.Add(kvp.Key, kvp.Value);
-                                }
-                                else
-                                {
-                                    newDict[kvp.Key] = kvp.Value;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            throw new InvalidCastException("List contains non-dictionary elements.");
-                        }
-                    }
-
-                    currentDict = newDict;
-                }
-            }
-        }
-    }
-
+    
     private async ValueTask<List<RuleResultTree>> ExecuteRules(List<RuleParameter> ruleParameters,
         List<ResourceRule> resourceRules)
     {
