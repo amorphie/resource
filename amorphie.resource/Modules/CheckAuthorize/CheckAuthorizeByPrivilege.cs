4e9a464800793a40d2a775c4bef200a08a6cb630
Tayfun Yılmaz <tayfunyilmas@gmail.com>
2024-07-19 11:42:18 +0300

    headers added in callapi method

--- amorphie.resource/Modules/CheckAuthorize/CheckAuthorizeByPrivilege.cs
+++ amorphie.resource/Modules/CheckAuthorize/CheckAuthorizeByPrivilege.cs
@@ -1,18 +1,19 @@
 using System.Text;
 using System.Text.RegularExpressions;
+using amorphie.resource.Helper;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 public class CheckAuthorizeByPrivilege : CheckAuthorizeBase, ICheckAuthorize
 {
     public async ValueTask<IResult> Check(
-                                    CheckAuthorizeRequest request,
-                                    ResourceDBContext context,
-                                    HttpContext httpContext,
-                                    string headerClientId,
-                                    IConfiguration configuration,
-                                    CancellationToken cancellationToken
-                             )
+        CheckAuthorizeRequest request,
+        ResourceDBContext context,
+        HttpContext httpContext,
+        string headerClientId,
+        IConfiguration configuration,
+        CancellationToken cancellationToken
+    )
     {
         var resource = await GetResource(context, request, cancellationToken);
 
@@ -22,15 +23,15 @@ public class CheckAuthorizeByPrivilege : CheckAuthorizeBase, ICheckAuthorize
         string allowEmptyPrivilege = configuration["AllowEmptyPrivilege"];
 
         var resourcePrivileges = await context!.ResourcePrivileges!.Include(i => i.Privilege)
-                        .AsNoTracking().Where(x => (
-                                                (x.ResourceId != null && x.ResourceId == resource.Id)
-                                                || 
-                                                (x.ResourceGroupId != null && x.ResourceGroupId == resource.ResourceGroupId)
-                                                )
-                                                   && (x.ClientId == null || x.ClientId.ToString() == headerClientId)
-                                                   && x.Status == "A")
-                        .OrderBy(x => x.Priority)
-                        .ToListAsync(cancellationToken);
+            .AsNoTracking().Where(x => (
+                                           (x.ResourceId != null && x.ResourceId == resource.Id)
+                                           ||
+                                           (x.ResourceGroupId != null && x.ResourceGroupId == resource.ResourceGroupId)
+                                       )
+                                       && (x.ClientId == null || x.ClientId.ToString() == headerClientId)
+                                       && x.Status == "A")
+            .OrderBy(x => x.Priority)
+            .ToListAsync(cancellationToken);
 
         if (resourcePrivileges == null || resourcePrivileges.Count == 0)
         {
@@ -74,19 +75,47 @@ public class CheckAuthorizeByPrivilege : CheckAuthorizeBase, ICheckAuthorize
                     privilegeUrl = privilegeUrl.Replace(variable.Key, variable.Value.ToString());
 
                 var apiClient = new HttpClient();
-
                 HttpResponseMessage response;
 
                 if (resourcePrivilege.Privilege.Type == amorphie.core.Enums.HttpMethodType.POST)
                 {
                     var data = string.IsNullOrEmpty(request.Data) ? "" : request.Data;
-                    var httpContent = new StringContent(data, Encoding.UTF8, "application/json");
+                    using var httpContent = new StringContent(data, Encoding.UTF8, "application/json");
+                    foreach (var item in httpContext.Request.Headers)
+                    {
+                        if (CallApiConsts.IgnoreDefaultHeaders.Contains(item.Key) ||
+                            CallApiConsts.ExcludeHeaders.Contains(item.Key.ToLower()))
+                        {
+                            continue;
+                        }
+
+                        if (!httpContent.Headers.Contains(item.Key))
+                        {
+                            httpContent.Headers.TryAddWithoutValidation(item.Key, item.Value.ToString());
+                        }
+                    }
 
                     response = await apiClient.PostAsync(privilegeUrl, httpContent);
                 }
                 else
                 {
-                    response = await apiClient.GetAsync(privilegeUrl);
+                    using HttpRequestMessage getRequest =
+                        new HttpRequestMessage(HttpMethod.Get, privilegeUrl);
+                    foreach (var item in httpContext.Request.Headers)
+                    {
+                        if (CallApiConsts.IgnoreDefaultHeaders.Contains(item.Key) ||
+                            CallApiConsts.ExcludeHeaders.Contains(item.Key.ToLower()))
+                        {
+                            continue;
+                        }
+
+                        if (!getRequest.Headers.Contains(item.Key))
+                        {
+                            getRequest.Headers.TryAddWithoutValidation(item.Key, item.Value.ToString());
+                        }
+                    }
+
+                    response = await apiClient.SendAsync(getRequest);
                 }
 
                 if (!response.IsSuccessStatusCode)
