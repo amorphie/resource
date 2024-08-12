6208eac02c2196a31f74ea62f3fa169d277be4a3
Tayfun Yılmaz <tayfunyilmas@gmail.com>
2024-08-12 09:01:48 +0300

    missing logging (request method) added

--- amorphie.resource/Modules/ResourcePrivilegeModule.cs
+++ amorphie.resource/Modules/ResourcePrivilegeModule.cs
@@ -19,35 +19,38 @@ public class ResourcePrivilegeModule : BaseBBTRoute<DtoResourcePrivilege, Resour
     }
 
     public async ValueTask<IResult> CheckAuthorize(
-         [FromBody] CheckAuthorizeRequest request,
-         [FromServices] ResourceDBContext context,
-         HttpContext httpContext,
-         [FromHeader(Name = "clientId")] string headerClientId,
-         [FromServices] IConfiguration configuration,
-         [FromQuery] string? checkAuthMethod,
-         ILogger<ResourcePrivilegeModule> logger,
-         CancellationToken cancellationToken
-         )
+        [FromBody] CheckAuthorizeRequest request,
+        [FromServices] ResourceDBContext context,
+        HttpContext httpContext,
+        [FromHeader(Name = "clientId")] string headerClientId,
+        [FromServices] IConfiguration configuration,
+        [FromQuery] string? checkAuthMethod,
+        ILogger<ResourcePrivilegeModule> logger,
+        CancellationToken cancellationToken
+    )
     {
         var transaction = Elastic.Apm.Agent.Tracer.CurrentTransaction;
         transaction.SetLabel("ClientId", headerClientId);
         transaction.SetLabel("RequestBody.Url", request.Url);
         transaction.SetLabel("RequestBody.Method", request.Method);
         transaction.SetLabel("RequestBody.Data", request.Data);
-        
+
         ICheckAuthorize checkAuthorize;
 
         if (checkAuthMethod == "Rule")
         {
-            logger.LogInformation($"Request.CheckAuthMethod:Rule | ClientId:{headerClientId} | Request.Url:{request.Url} | Request.Data:{request.Data}");
+            logger.LogInformation(
+                $"Request.CheckAuthMethod:Rule | ClientId:{headerClientId} | Request.Url:{request.Url} | Request.Data:{request.Data} | Request.Method:{request.Method}");
             checkAuthorize = new CheckAuthorizeByRule();
         }
         else
         {
-            logger.LogInformation($"Request.CheckAuthMethod:None | ClientId:{headerClientId} | Request.Url:{request.Url} | Request.Data:{request.Data}");
+            logger.LogInformation(
+                $"Request.CheckAuthMethod:None | ClientId:{headerClientId} | Request.Url:{request.Url} | Request.Data:{request.Data} | Request.Method:{request.Method}");
             checkAuthorize = new CheckAuthorizeByPrivilege();
         }
 
-        return await checkAuthorize.Check(request, context, httpContext, headerClientId, configuration, logger, cancellationToken);
+        return await checkAuthorize.Check(request, context, httpContext, headerClientId, configuration, logger,
+            cancellationToken);
     }
 }
